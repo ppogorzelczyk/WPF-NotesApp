f693ef9d17e9e2b5b34bb4c9ea966f7c55ca8394
Patryk Pogorzelczyk <patryk.pogorzelczyk@ordersoft.pl>
2020-03-31 13:59:03 +0200

    Speech Recognition added

--- NotesApp/NotesApp/View/NotesWindow.xaml.cs
+++ NotesApp/NotesApp/View/NotesWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,9 +22,42 @@ namespace NotesApp.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        SpeechRecognitionEngine recognizer;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            
+
+            //english culture
+            RecognizerInfo ri = null;
+            foreach (var i in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if (i.Culture.TwoLetterISOLanguageName.Equals("en"))
+                {
+                    ri = i;
+                }
+            }
+            recognizer = new SpeechRecognitionEngine(ri);
+
+            //nie działa w Polsce
+            //var currentCulture = SpeechRecognitionEngine.InstalledRecognizers().Where(r => r.Culture.Equals(Thread.CurrentThread.CurrentCulture)).FirstOrDefault();
+            //recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            GrammarBuilder builder = new GrammarBuilder();
+            builder.AppendDictation();
+            builder.Culture = recognizer.RecognizerInfo.Culture; //for english culture
+            Grammar grammar = new Grammar(builder);
+            recognizer.LoadGrammar(grammar);
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text;
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -29,9 +65,17 @@ namespace NotesApp.View
             Application.Current.Shutdown();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
-
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+            if(!isButtonEnabled)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                recognizer.RecognizeAsyncStop();
+            }
         }
 
         private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
